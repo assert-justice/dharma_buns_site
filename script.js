aeac8d1ed65f3e53f9c1b898f07cf8fdc9781659
Riley Miller <waskerdu@gmail.com>
2022-08-09 13:32:06 -0700

    removed function implementation!

--- script.js
+++ script.js
@@ -19,9 +19,6 @@ function main() {
   /*
     Set up the submit handler event listener and call render for the first time.
   */
-  const form = document.querySelector("form");
-  form.addEventListener("submit", submitHandler);
-  render();
 }
 
 function submitHandler(event) {
@@ -33,13 +30,6 @@ function submitHandler(event) {
     - add a new review object to the reviews global variable
     - and rerender the page.
   */
-  event.preventDefault();
-  const title = document.querySelector("#title").value;
-  const rating = document.querySelector("#rating").value;
-  const text = document.querySelector("#body").value;
-  const review = {title, rating, text};
-  reviews.push(review);
-  render();
 }
 
 function render() {
@@ -47,21 +37,12 @@ function render() {
     The render method should first empty the contents of the reviews ul and then append each review to it.
     You'll want to call the reviewTemplate function below.
   */
-  const container = document.querySelector("#reviews");
-  container.innerHTML = "";
-  reviews.map(reviewTemplate).forEach(rev => container.innerHTML += rev);
 }
 
 function reviewTemplate(review) {
   /*
     Given a review object this function should return the appropriately formatted html for the review as a string.
   */
-  const {title, rating, text} = review;
-  return `<li>
-        <h2>${title}</h2>
-        <h3>${rating}/5 Stars</h3>
-        <p>${text}</p>
-      </li>`;
 }
 
 addEventListener("load", main);
